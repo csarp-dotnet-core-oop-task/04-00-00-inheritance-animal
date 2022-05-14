86bd5dabc52a3b9c5bc06c9544638f2c48c98874
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-05-14 08:58:06 +0200

    Az induló feladat

---
+++ AnimalTradeProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using oop.model;
+
+namespace AnimalTradeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // I1.13 Tesztkód
+
+            // Animal a = new Animal();
+            Dog f = new Dog("Frakk", 3, "németjuhász");
+            f.Greet();
+            f.MyDatas();
+
+            // I1.14 Nézzük meg az osztályok UML diagramját
+
+            // I1.15 Készítsük el a macska osztályt, amely szintén állat, és jellemzője a bundájának a színe
+        }
+    }
+}
